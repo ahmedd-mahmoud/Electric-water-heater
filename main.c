70ed01c68cb909eb6604a6b831b42b3243f50835
ahmedd-mahmoud <ahmed.mahran831@gmail.com>
2022-12-26 20:31:33 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 12/26/2022 8:14:25 PM
+ *  Author: DELL
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
